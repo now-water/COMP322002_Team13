d6a433d7410ad058501c009a344d97b7abc5164e
unknown <ljy2784437@gmail.com>
2020-11-23 01:58:17 +0900

    Phase4

---
+++ Phase4/README.md
@@ -0,0 +1,2 @@
+<h3>Login page</h3>
+<image src="https://user-images.githubusercontent.com/51476083/98394486-302b5580-209e-11eb-80b8-da32734aba0c.png">
---
+++ Phase4/src/JDBCTEST.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+public class JDBCTEST {
+    public static void main(String[] args) {
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:postgresql://localhost/phase3";
+        String user = "postgres";
+        String password = "kwon0879";
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            st = conn.createStatement();
+            rs = st.executeQuery("SELECT * FROM \"knuMovie\".\"ACCOUNT\"");
+
+            while (rs.next())
+                System.out.println(
+                        rs.getString(1) + ' ' +
+                                rs.getString(2) + ' ' +
+                                rs.getString(3) + ' ' +
+                                rs.getString(4) + ' ' +
+                                rs.getDate(5) + ' ' +
+                                rs.getInt(6) + ' ' +
+                                rs.getString(7) + ' ' +
+                                rs.getString(8) + ' ' +
+                                rs.getString(9) + ' ' +
+                                rs.getString(10) + ' ' +
+                                rs.getBoolean(11));
+        } catch (SQLException sqlEX) {
+            System.out.println(sqlEX);
+        } finally {
+            try {
+                rs.close();
+                st.close();
+                conn.close();
+            } catch (SQLException sqlEX) {
+                System.out.println(sqlEX);
+            }
+        }
+    }
+}

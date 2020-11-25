944689d6251937a0c16cf275f04a3a12e93b606a
nowwater <nowwater0225@gmail.com>
2020-11-26 00:22:44 +0900

    Added main form

--- Phase4/main.js
+++ Phase4/main.js
@@ -7,10 +7,22 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 app.get('/', function(req, res){
-    res.render("login");
+    res.render("index");
+})
+app.get('/index', function(req, res){
+    res.render("index");
 })
 app.get('/register', function(req, res){
     res.render("signUp");
+})
+app.get('/login', function(req, res){
+    res.render("login");
+})
+app.get('/form', function(req, res){
+    res.render("form");
+})
+app.get('/dashboard', function(req, res){
+    res.render("dashboard");
 })
 app.listen(3000, function () {
     console.log('Connected 3000 ! ')

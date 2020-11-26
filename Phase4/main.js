a2ea677d0b9f4f87d93fc9a2c5e7e733a8e229fa
nowwater <nowwater0225@gmail.com>
2020-11-27 01:24:00 +0900

    Updated Session function

--- Phase4/main.js
+++ Phase4/main.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const session = require('express-session');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
-
+app.use(session({
+    secret: 'keyboard cat',  // 암호화
+    resave: false,
+    saveUninitialized: true
+}));
 var movie_main = require('./routes/routes')();
 
 
--- Phase4/routes/routes.js
+++ Phase4/routes/routes.js
@@ -1,45 +1,105 @@
 const e = require('express');
-const session = require('express-session');
 
 module.exports = function () {
     var route = e.Router();
     var conn = require('../config/db')();
     var bodyParser = require('body-parser');
-    const { response } = require("express");
-    route.use(bodyParser.urlencoded({ extended: true }));
-    var user = {
+    const {response} = require("express");
+    route.use(bodyParser.urlencoded({extended: true}));
 
-    }
     route.get('/', (req, res) => {
         res.redirect('/login');
     })
     route.get('/index', (req, res) => {
-        var sql = 'SELECT * FROM \"knuMovie\".\"MOVIE\"';
+        if (!req.session.isLogined) {
+            res.redirect('login');
+        }
+
+        var sql = 'SELECT * FROM \"knuMovie\".\"MOVIE\" M, \"knuMovie\".\"RATING\" R ' +
+            "WHERE M.title =  " +
+            "ACC.acc_id != \'" + `${req.session.user_id}` + "\' ";
         conn.query(sql, (err, results) => {
             if (err) {
                 console.log(err);
                 res.status(500).send("DB Error");
             }
-            res.render('index', { results: results.rows });
+            console.log(req.session);
+            //console.log(`${req.session.num}`);
+            //console.log(`${req.session.user_id}`);
+            res.render('index', {
+                results: results.rows,
+                session: `${req.session.user_id}`
+            });
         });
     })
     route.get('/register', (req, res) => {
         res.render("signUp");
     })
     route.get('/login', (req, res) => {
+        // 한 클라이언트는 중복 로그인이 불가능하다.
+        if (req.session.isLogined)
+            res.redirect('index');
+
+        if (!req.session.num) {
+            req.session.num = 1;
+        } else {
+            req.session.num = req.session.num + 1;
+        }
         res.render("login");
     })
     route.post('/login', (req, res) => {
         var user_id = req.body.user_id;
         var user_pw = req.body.user_pw;
         console.log(user_id + " " + user_pw);
+        var sql = "SELECT * " +
+            "FROM \"knuMovie\".\"ACCOUNT\" AS A " +
+            "WHERE A.acc_id = " + "\'" + user_id + "\' ";
+        console.log(sql);
 
+        var success = false;
         // DB 처리
-        //req.session.user_id = user_id;
-        //console.log(req.session);
-        res.render("index");
+        var canLogin = false;
+        conn.query(sql)
+            .then(queryRes => {
+                const rows = queryRes.rows;
+                //console.log(rows);
+                rows.map(row => {
+                    //console.log(`Read: ${JSON.stringify(row)}`);
+                    if (user_id === row.acc_id && user_pw === row.acc_pw) {
+                        // 유저의 모든 정보 저장하기
+                        req.session.user_id = row.acc_id;
+                        req.session.pw = row.acc_pw;
+                        req.session.name = row.user_name;
+                        req.session.phone_num = row.phone_num;
+                        req.session.birth = row.birth_date;
+                        req.session.age = row.age;
+                        req.session.gender = row.gender;
+                        req.session.address = row.address;
+                        req.session.job = row.job;
+                        req.session.mem_type = row.mem_type;
+                        req.session.manager = row.manager;
+                        req.session.isLogined = true;
+                        //console.log("Session test : ");
+                        //console.log(req.session);
+
+                        res.redirect('index');
+                    } else {
+                        res.send('<script type="text/javascript">alert("로그인 정보가 없습니다.");' +
+                            'window.location.href="http://localhost:3000/login";</script>');
+                        console.log("로그인 실패");
+                    }
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    })
+    route.get('/logout', (req, res) => {
+        req.session.destroy();
+        res.redirect('login');
     })
     route.get('/form', (req, res) => {
+        console.log(`${req.session.user_id}`);
         res.render("form");
     })
 
